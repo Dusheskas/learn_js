f8c48fba4900d6ec82f4dc7068641a76931b987c
Andrew <andrew.salomatinn@gmail.com>
2023-02-18 16:58:21 +0200

    Practise 3, функции, массивы, циклы

--- js/practise.js
+++ js/practise.js
@@ -1,5 +1,6 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+"use strict"
 
+let numberOfFilms;
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,12 +8,56 @@ const personalMovieDB = {
     genres: [],
     private: false
 }
-
-for (let i = 0; i < personalMovieDB.count; i++) {
-    const movieName = prompt("Один из последних фильмов?");
-    const movieGrade = +prompt("На сколько его оцените?");
-
-    personalMovieDB.movies[movieName] = movieGrade;
+function start(){
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+    
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+    }
 }
 
-console.dir(personalMovieDB);
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < personalMovieDB.count; i++) {
+        const movieName = prompt("Один из последних фильмов?"),
+              movieGrade = +prompt("На сколько его оцените?");
+        
+        if(movieName == null || movieName.length>50 || movieName == ' ') {
+            i--;
+        }
+        else {
+            personalMovieDB.movies[movieName] = movieGrade;
+        }
+    }
+}
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Ты кринжа");
+    }
+    else if(10 <= personalMovieDB.count < 30) {
+        alert("Классик");
+    }
+    else {
+        alert("Красавэлла");
+    }
+}
+
+function showMyDB() {
+    if(personalMovieDB.private == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`${i}-й ваш любимый жанр?`);
+    }
+}
+
+start();
+writeYourGenres();
+// rememberMyFilms();
+detectPersonalLevel();
+

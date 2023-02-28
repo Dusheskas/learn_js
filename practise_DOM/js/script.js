f13e119cd00d8dd4152803cff6bce90036a03c8a
Andrew <andrew.salomatinn@gmail.com>
2023-02-28 11:01:44 +0200

    Делаю практическую

--- practise_DOM/js/script.js
+++ practise_DOM/js/script.js
@@ -1,49 +1,56 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 "use strict";
 
 const movieDB = {
-   movies: [
-      "Логан",
-      "Лига справедливости",
-      "Ла-ла лэнд",
-      "Одержимость",
-      "Скотт Пилигрим против...",
-   ],
+	movies: [
+		"Логан",
+		"Лига справедливости",
+		"Ла-ла лэнд",
+		"Одержимость",
+		"Скотт Пилигрим против...",
+	],
 };
 
 const adv = document.querySelectorAll(".promo__adv img"),
-   movieBg = document.querySelector(".promo__bg"),
-   movieGenre = movieBg.querySelector(".promo__genre"),
-   moviesList = document.querySelector(".promo__interactive-list");
+	movieBg = document.querySelector(".promo__bg"),
+	movieGenre = movieBg.querySelector(".promo__genre"),
+	moviesList = document.querySelector(".promo__interactive-list");
 
 adv.forEach((item) => {
-   item.remove();
+	item.remove();
 });
 
 movieGenre.textContent = "драма";
 
 movieBg.style.cssText = 'background: url("./img/bg.jpg") center top no-repeat;';
 
-moviesList.innerHTML = ""; // Удалили статичный список
-movieDB.movies.sort(); // Сортируем фильмы по алфавиту
+refreshMovieList();
 
-movieDB.movies.forEach((film, i) => {
-   moviesList.innerHTML += `
-		<li class="promo__interactive-item">${i + 1}. ${film}
-			<div class="delete"></div>
-		</li>
-	`;
+function refreshMovieList() {
+	moviesList.innerHTML = ""; // Удалили статичный список
+	movieDB.movies.sort(); // Сортируем фильмы по алфавиту
+
+	movieDB.movies.forEach((film, i) => {
+		moviesList.innerHTML += `
+         <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+         </li>
+      `;
+	});
+}
+
+// =-=-=-=-=--=-=-
+const newMovieName = document.querySelector(".adding__input"),
+	addMovieBtn = document.querySelector("#addMovie");
+
+addMovieBtn.addEventListener("click", (e) => {
+	e.preventDefault(); // Отменяем перезагрузку страницы
+	let movieName = newMovieName.value;
+
+	if (movieName.length > 5) {
+		movieName = movieName.slice(0, 5) + "...";
+
+		// console.log(movieName);
+	}
+	movieDB.movies.push(newMovieName.value);
+	refreshMovieList();
 });
